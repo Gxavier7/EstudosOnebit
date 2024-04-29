441da847db8912ac1a8a6d8d687334845caf4e35
Gabriel Martins Xavier <gabrim0070@gmail.com>
2024-04-29 10:41:18 -0300

    Algumas aulas de Typescript

---
+++ Typescript-aulas/Evitando-Tipagem/index.ts
@@ -0,0 +1,7 @@
+function sendSpaceship(spaceship:{ pilot: string, copilot?: string}) {
+    //.....
+}
+
+sendSpaceship({ pilot: `Han Solo`, copilot: `Chewbaca` })
+
+sendSpaceship({ pilot: `Luke` })
