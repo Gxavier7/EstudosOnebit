554b8f4340d6b6e99b9362782bc6b3c48f678b2f
Gabriel Martins Xavier <gabrim0070@gmail.com>
2024-11-26 14:58:39 -0300

    configurando o sequelize novamente

--- OneBitFlix/src/server.ts
+++ OneBitFlix/src/server.ts
@@ -1,19 +1,13 @@
 import express from "express"
 import { sequelize } from "./database"
-import { adminJs, adminJsRouter } from "./adminjs"
 
 const app = express()
 
-app.use(express.static('public'))
+const PORT = process.env.PORT || 3000
 
-app.use(adminJs.options.rootPath, adminJsRouter)
-
-const PORT = process.env.PORT || 3000 
-
-app.listen( PORT, () => {
-  sequelize.authenticate().then( () => {
-    console.log(`DB connection seccessfull`)
+app.listen(PORT, () => {
+  sequelize.authenticate().then(() => {
+    console.log('DB connection successfull');
   })
-
-  console.log(`Server started successfuly at port http://localhost:${PORT}`);
+  console.log(`Server started successfully at http://localhost:${PORT}`);
 })
