5f7ae30798a6fd4f514726326186d9841ab2ba64
Gabriel Martins Xavier <gabrim0070@gmail.com>
2025-01-24 17:35:29 -0300

    obtendo 3 cursos em destaque

--- onebitflix/src/controllers/coursesController.ts
+++ onebitflix/src/controllers/coursesController.ts
@@ -2,6 +2,21 @@ import { Request, Response } from "express";
 import { courseService } from "../services/coursesService";
 
 export const coursesController = {
+  // GET /courses/featured
+  featured: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const featuredCourses = await courseService.getRandomfeaturedCourses()
+
+      res.json(featuredCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
   // GET /courses/:id
   show: async (req: Request, res: Response) => {
     const { id } = req.params
