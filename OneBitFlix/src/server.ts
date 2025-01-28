63c0a0f36b931f4f7c28c249389800fa8e3d6e2d
Gabriel Martins Xavier <gabrim0070@gmail.com>
2025-01-28 13:57:54 -0300

    registro de usuários

--- OneBitFlix/src/server.ts
+++ OneBitFlix/src/server.ts
@@ -6,7 +6,11 @@ import { router } from "./routes"
 const app = express()
 
 app.use(express.static('public'))
+
+app.use(express.json())
+
 app.use(adminJs.options.rootPath, adminJsRouter)
+
 app.use(router)
 
 const PORT = process.env.PORT || 3000
