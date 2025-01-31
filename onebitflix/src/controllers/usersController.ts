781fc225289028300c5795d7aafc5b9899c1e258
Gabriel Martins Xavier <gabrim0070@gmail.com>
2025-01-31 13:50:37 -0300

    obtendo informações do usuário e atualizando gitignore

--- onebitflix/src/controllers/usersController.ts
+++ onebitflix/src/controllers/usersController.ts
@@ -3,6 +3,21 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
+  // GET /users/current
+  show: async ( req: AuthenticatedRequest, res: Response ) => {
+    
+    try {
+      const currentUser = req.user!
+      
+      return res.json(currentUser)
+
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
   // GET /users/current/watching
   watching: async ( req: AuthenticatedRequest, res: Response ) => {
     const { id } = req.user!
@@ -16,5 +31,6 @@ export const usersController = {
         return res.status(400).json({ message: err.message })
       }
     }
-  }
+  },
+
 }
